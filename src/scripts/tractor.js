3508611cb421263467fdb00760f2548f31eb82e3
Colin Arel <romans_one16@live.com>
2020-08-30 16:39:11 -0500

    completed sowing the field

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,7 +1,41 @@
+// import { createPlan } from './plan.js';
+import { addPlant, usePlants } from './field.js';
+import { createAsparagus } from "./seeds/asparagus.js"
+import { createCorn } from "./seeds/corn.js"
+import { createPotato } from "./seeds/potato.js"
+import { createSoybean } from "./seeds/soybean.js"
+import { createSunflower } from "./seeds/sunflower.js"
+import { createWheat } from "./seeds/wheat.js"
+
+let planArray = [];
+
 export const plantSeeds = (plantingPlan) => {
-    for (const arrayOfPlants of plantingPlan) {
-        for (const plant of arrayOfPlants) {
-            
+    for (const row of plantingPlan) {
+        for (const plant of row) {
+            if (plant === "Asparagus") {
+                const asparagus = createAsparagus();
+                addPlant(asparagus);
+            }
+            else if (plant === "Potato") {
+                const potato = createPotato();
+                addPlant(potato);
+            }
+            else if (plant === "Soybean") {
+                const soybean = createSoybean();
+                addPlant(soybean);
+            }
+            else if (plant === "Sunflower") {
+                const sunflower = createSunflower();
+                addPlant(sunflower);
+            }
+            else if (plant === "Wheat") {
+                const wheat = createWheat();
+                addPlant(wheat);
+            }
+            else {
+                const corn = createCorn();
+                addPlant(corn);
+            }
         }
     }
 }
